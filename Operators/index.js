a7a13e48d6f79c052849c421441cbe764da799fe
harry <harrywinchester777@gmail.com>
2023-08-14 22:13:18 +0630

    Operators

---
+++ Operators/index.js
@@ -0,0 +1,74 @@
+// expression
+// 1+6
+// 7-1
+// "a"+"b"
+
+// statement
+// let exp=1+2;
+// let exp1="a"+"b";
+
+// OPERATORS 
+// 1.arithmetic 
+// 2.assignment
+// 3.comparison
+// 4.logical
+
+// arithemetic 
+// let ari="a"+"b";
+// console.log(ari);
+
+// let ari1=7+"y";
+// console.log(ari1);
+
+// let ari2=5-2;
+// console.log(ari2);
+
+// let ari3="5"-"3";
+// console.log(ari3);
+
+// let ari4="y"-"e";
+// console.log(ari4);
+
+// let ari5="y"-"5";
+// console.log(ari5);
+
+// let y=3*5;
+// console.log(y);
+
+// let x=6/2;
+// console.log(x);
+
+// let z=6%4;
+// console.log(z);
+
+// let a,b,c=0;
+// console.log(a);
+// console.log(b);
+// console.log(c);
+
+// power 
+// console.log(3**3);
+
+// let bread = 4;
+// bread+=1;
+// bread-=2;
+// console.log(bread);
+
+// let a=4;
+// a+=4;
+// console.log(a);
+// a-=4;
+// console.log(a);
+// a*=2;
+// console.log(a);
+// a/=2;
+// console.log(a);
+// a%=2;
+// console.log(a);
+
+// console.log(!true);
+
+// let num=2;
+// console.log(num>0 && num==2);
+
+// console.log(num>5 || num==2);
