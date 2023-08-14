05f33760480242f4ee68b2f1d4163e249b89eb80
harry <harrywinchester777@gmail.com>
2023-08-14 11:16:56 +0630

    Data Types

---
+++ Data Types/index.js
@@ -0,0 +1,57 @@
+// let greet="Hello World";
+// console.log(greet);
+
+// let myName="My name is Ye Lin Soe.";
+// console.log(myName);
+
+// let myProfession=`${greet} I am a Programmer.`;
+// console.log(myProfession);
+
+// let myNP=myName + " " + myProfession;
+// console.log(myNP);
+
+// let myBirthday='My Birthday is on 17th May.';
+// console.log(myBirthday);
+
+// let myAge=23;
+// console.log(myAge);
+// console.log(typeof myAge);
+
+// let time='3 o\'clock';
+// console.log(time);
+
+// let myHeight="5'8\"";
+// console.log(myHeight);
+
+// let num1 = 5;
+// let num2 = 6;
+// console.log(num1+num2);
+
+//Boolean
+// let myVar=false;
+// console.log(Boolean(myVar));
+
+// let myVar1="false";
+// console.log(Boolean(myVar1));
+
+// let myVar2=-1;
+// console.log(Boolean(myVar2));
+
+// let myVar3=0;
+// console.log(Boolean(myVar3));
+
+// undefined
+let x;
+console.log(Boolean(x));
+
+// let x1 = undefined;
+// console.log(x1);
+
+// null
+let y=null;
+console.log(Boolean(y));
+
+// Not A Number (NaN)
+let num1;
+let num2 = 1;
+console.log(Boolean(num1+num2));
